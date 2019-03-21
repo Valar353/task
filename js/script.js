fc8aa55f66609d94f0d58aa7e0263c32c307c817
Valar353 <ilya.belikov97@mail.ru>
2019-03-21 16:58:15 +0300

    oop4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+const currentTime = document.getElementsByClassName('currentTime')[0];
+const table = document.getElementsByTagName('tbody')[0];
+const all = document.getElementsByClassName('allTime')[0];
+
+
+let objTimer = new Timer(currentTime,table, all);
+
+document.getElementById('timeTracking').addEventListener('click', function(e){
+  const target = e.target;
+  if(target.className == 'btnstart'){
+    if(!objTimer.timer){
+      objTimer.initTimer();
+    }
+  }
+  if(target.className == 'btnstop'){
+    if(objTimer.timer){
+      objTimer.stopTimer();
+    }
+  }
+  if(target.className == 'del'){
+    objTimer.removeChild(target);
+    objTimer.printAllTime();
+  }
+  if(target.className == 'title'){
+    const result = prompt('Назовите интервал', 'Интервал '+objTimer.i);
+    target.innerHTML = result;
+  }
+});
+
+
+
+
+
+
 
 class Timer
 {
@@ -81,33 +115,3 @@ class Timer
     this.all.innerHTML = this.allTime[0] + ':'+ this.allTime[1];
   }
 }
-
-
-const currentTime = document.getElementsByClassName('currentTime')[0];
-const table = document.getElementsByTagName('tbody')[0];
-const all = document.getElementsByClassName('allTime')[0];
-
-
-let objTimer = new Timer(currentTime,table, all);
-
-document.getElementById('timeTracking').addEventListener('click', function(e){
-  const target = e.target;
-  if(target.className == 'btnstart'){
-    if(!objTimer.timer){
-      objTimer.initTimer();
-    }
-  }
-  if(target.className == 'btnstop'){
-    if(objTimer.timer){
-      objTimer.stopTimer();
-    }
-  }
-  if(target.className == 'del'){
-    objTimer.removeChild(target);
-    objTimer.printAllTime();
-  }
-  if(target.className == 'title'){
-    const result = prompt('Назовите интервал', 'Интервал '+objTimer.i);
-    target.innerHTML = result;
-  }
-});
